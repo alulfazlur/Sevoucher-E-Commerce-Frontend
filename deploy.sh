a63c96787d912a483bdadd31a26252dcda0fddfd
Fazlur Rahman <fazlur@alterra.id>
2020-06-08 11:35:43 +0700

    travis

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+sudo docker build -f "Dockerfile" -t alulfazlur/react-tutorial:$TRAVIS_BUILD_NUMBER .
+sudo docker push alulfazlur/react-tutorial:$TRAVIS_BUILD_NUMBER
+
+echo $GCLOUD_SERVICE_KEY_PRD | base64 --decode > ${HOME}/gcloud-service-key.json
+gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
+
+gcloud container clusters get-credentials $CLUSTER_NAME_PRD --region $CLOUDSDK_COMPUTE_REGION --project $PROJECT_NAME_PRD
+
+kubectl config view
+kubectl config current-context
+
+ ## deploy api server
+kubectl -n alta10 set image deployment/sevoucher-fe sevoucher-fe=alulfazlur/react-tutorial:$TRAVIS_BUILD_NUMBER
