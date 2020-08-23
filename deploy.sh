fa7f7a1cdfa1dfe4a353c098ad52e0f569a34c47
Fazlur Rahman <fazlur@alterra.id>
2020-08-23 16:13:54 +0700

    GCP

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,11 @@
-sudo docker build -f "Dockerfile" -t alulfazlur/sevoucher-fe:$TRAVIS_BUILD_NUMBER .
-sudo docker push alulfazlur/sevoucher-fe:$TRAVIS_BUILD_NUMBER
+#!/bin/bash
 
-echo $GCLOUD_SERVICE_KEY_PRD | base64 --decode > ${HOME}/gcloud-service-key.json
-gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
+eval "$(ssh-agent -s)" &&
+ssh-add -k ~/.ssh/id_rsa &&
 
-gcloud container clusters get-credentials $CLUSTER_NAME_PRD --region $CLOUDSDK_COMPUTE_REGION --project $PROJECT_NAME_PRD
-
-kubectl config view
-kubectl config current-context
-
- ## deploy api server
-kubectl -n alta10 set image deployment/sevoucher-fe sevoucher-fe=alulfazlur/sevoucher-fe:$TRAVIS_BUILD_NUMBER
+source ~/.profile
+echo "$DOCKER_PASSWORD" | docker login --username $DOCKER_USERNAME --password-stdin
+sudo docker stop sevoucher-fe
+sudo docker rm sevoucher-fe
+sudo docker rmi alulfazlur/sevoucher-fe:latest
+sudo docker run -d --name sevoucher-fe -p 8443:80 alulfazlur/sevoucher-fe:latest
